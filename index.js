dcbae9b3d19064cd5fd7a2ca4942dbcf27b7bf70
Keith Fernandes <keith_kcf@hotmail.com>
2021-12-23 09:01:46 +0000

    app ready to run

--- index.js
+++ index.js
@@ -4,4 +4,29 @@ const   http = require('http'), // this module provides the HTTP server function
         fs  = require ('fs'),// this module allows to work with the file system read and write files back
         xmlParse = require ('xslt-processor'), xmlParse,// this module allows to work with XML files
         xsltProcess = requirre('xslt-processor'), xsltProcess,//the same module allows us to utilise XSL trasnformations
-        xml2js = require('xml2js')// this module does XML<->JSON converion
+        xml2js = require('xml2js');// this module does XML<->JSON converion
+
+const   router = express(), 
+        server = http.createServer(router);
+
+router.get('/', function(req, res){
+
+    res.writeHead[200, {'Content-Type' : 'text/html'}];
+
+    let xml = fs.readFileSync('TripInnTrip.xml', 'utf8'), 
+        xsl = fs.readFileSync('TripInnTrip.xml', 'utf8');
+
+    let doc = xmlParse(xml),
+        stylesheet = xmlParse(xsl);
+
+    let result = xsltProcess(doc, stylesheet);
+    
+    res.end(result.toString());
+
+});        
+
+server.listen[process.env.PORT||3000, process.env.IP||"0.0.0.0", function()
+{
+    const addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port)
+}];
