7b7a4669f1965af42c2abdaeb35e9a30c56b9e8d
Keith Fernandes <keith_kcf@hotmail.com>
2022-01-06 21:36:35 +0000

    Adding details

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ const   router = express(),
         server = http.createServer(router);
 
     router.use(express.static(path.resolve(__dirname,'views'))); //Serving static content from "views" folder
+    router.use(express.urlencoded({extended: true})); //allow the data send from the client to be encoded in a URL targeting our end point
+    router.use(express.json());//include support for JSON
+
+    function XMLtoJSON(filename, cb){
+        let filepath = path.normalize(path.join(__dirname, filename));
+        fs.readFile(filepath, 'utf8', function(err, xmlStr){
+            if (err) throw (err);
+            xml2js.parseString(xmlStr, {}, cb);
+        });
+    };
+
+    function JSONtoXML(filename, obj, cb) {
+        let filepath = path.normalize(path.join(__dirname, filename));
+        let builder = new xml2js.Builder();
+        let xml = builder.buildObject(obj);
+        fs.unlinkSync(filepath);
+        fs.writeFile(filepath, xml, cb);
+    };
 
     router.get('/get/html', function(req, res) {
 
@@ -32,6 +50,34 @@ const   router = express(),
     console.log(result);
     
     res.end(result.toString());
+
+});
+
+router.post('/post/json', function(req, res) {
+
+    console.log(req.body);
+
+    function appendJSON(obj) {
+
+        console.log(JSON.stringify(obj, null, " "))
+
+        XMLtoJSON('TripInnTrip.xml', function (err, result){
+            if (err) throw (err);
+
+            result.trip.section[obj.sec_n].tour.push({'place': obj.item, 'price': obj.price});
+
+            console.log(JSON.stringify(result, null, " "));
+
+            JSONtoXML('TripInnTrip.xml', result, function(err){
+                if (err) console.log(err);
+            });
+
+        });
+
+    };
+    appendJSON(req.body);
+
+    res.redirect('back');
 
 });
 
--- views/js/table.js
+++ views/js/table.js
@@ -7,11 +7,27 @@ $.getHTMLuncached = function(url){
         cache: false, 
         success: function(html){
             $("#results").append(html)
+            select_row();
             }
         });
     };
 $.getHTMLuncached("/get/html");
 };
+
+function select_row()
+{
+    $("#menuTravel tbody tr[id]").click(function ()
+    {
+        $(this).addClass("selected");
+        var sec = $(this).prevAll("tr").children("td[colspan='3']").length - 1;
+        var ent = $(this).attr("id") - 1;
+        delete_row(sec, ent);
+    })
+
+};    
+
+    
+
 
 $(document).ready(function(){
     draw_table();
